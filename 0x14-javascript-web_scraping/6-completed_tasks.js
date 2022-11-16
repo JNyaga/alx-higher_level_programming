6e5809182a78df7f6f7ef292f68e6eace2c1cac3
Muriithi Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-11-16 03:02:24 +0300

    push api json tasks coompleted

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
 // A script that computes the number of tasks completed by user id.
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, resp, body) => {
+  if (err) { console.log(err); }
+
+  const completed = {};
+  const jsonBody = JSON.parse(body);
+  for (const task of jsonBody) {
+    if (task.completed) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
       }
     }
-    console.log(dash);
   }
+  console.log(completed);
 });
